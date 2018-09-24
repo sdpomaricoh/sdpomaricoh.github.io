a26df068bb0359922ee746ffeb3eac2b816c26f7
Sergio Pomárico <sergiodavid21@gmail.com>
2018-09-23 20:20:25 -0500

    actualizar curriculum

--- js/main.js
+++ js/main.js
@@ -1,1 +1,109 @@
-$(document).ready(function(){new WOW().init();});count=0;$(".menu li a").click(function(){count++;if(count>0){id=$(this).attr("id");if(id=="menu-about"){$("#about").removeClass("wow fadeInUp");$("#about").removeAttr("style");}}});$(window).load(function(){$('#preloader').delay(900).fadeOut('slow');});$("#html").animate({width:"90%"},5000);$("#photoshop").animate({width:"60%"},5000);$("#css").animate({width:"70%"},5000);$("#javascript").animate({width:"65%"},5000);$("#php").animate({width:"50%"},5000);$("#node").animate({width:"57%"},5000);$(".services .service i").mouseover(function(){$(this).addClass("animated bounce");});$(".services .service i").mouseout(function(){$(this).removeClass("animated bounce");});$("#back-button").click(function(){window.location.href="/#blog?=blog";});function getGET(){var loc=document.location.href;if(loc.indexOf('?')>0){var getString=loc.split('?')[1];var GET=getString.split('&');var get={};for(var i=0,l=GET.length;i<l;i++){var tmp=GET[i].split('=');get.url=unescape(decodeURI(tmp[1]));}return get;}}get=getGET();if(get){if(get.url=="blog"){console.log("estas en el blog");$('#tab-container').easytabs('select','#blog');}}$(document).ready(function(){$('#tab-container').easytabs({updateHash:false,animate:true,transitionIn:'fadeIn',transitionOut:'fadeOut',tabActiveClass:'active',transitionInEasing:'linear',transitionOutEasing:'linear'});$('#wrapper').bind('easytabs:after',function(){var myOptions={zoom:14,center:new google.maps.LatLng(6.208892,-75.5776498),mapTypeId:google.maps.MapTypeId.ROADMAP,scrollwheel:false,mapTypeControl:true,styles:[{"featureType":"administrative","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"administrative.country","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"administrative.province","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#e3e3e3"}]},{"featureType":"landscape.natural","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#afd7a2"},{"visibility":"on"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#91b384"}]},{"featureType":"road","elementType":"all","stylers":[{"color":"#cccccc"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"transit.station.airport","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"transit.station.airport","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#FFFFFF"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]}]};var map=new google.maps.Map(document.getElementById("map"),myOptions);var marker=new google.maps.Marker({map:map,position:new google.maps.LatLng(6.2735012,-75.5814543)});var infowindow=new google.maps.InfoWindow({content:"<b>Cl. 73 #67-43 </b><br/> Medellín, Antioquia"});google.maps.event.addListener(marker,"click",function(){infowindow.open(map,marker);});infowindow.open(map,marker);});});$(window).load(function(){$('#wrapper').bind('easytabs:after',function(){$(".portfolio_items").isotope({itemSelector:'.single_item',layoutMode:'fitRows',columnWidth:'.col-sm-3'});$('.portfolio_filter ul li').click(function(){$(".portfolio_filter ul li").removeClass("select-cat");$(this).addClass("select-cat");var selector=$(this).attr('data-filter');$(".portfolio_items").isotope({filter:selector,animationOptions:{duration:750,easing:'linear',queue:false,}});return false;});$('.image-link').magnificPopup({type:'image'});});});
+$(document).ready(function() {
+    new WOW().init();
+});
+
+count=0;
+
+$(".menu li a").click(function() {
+    count++;
+    if(count>0) {
+        id=$(this).attr("id");
+        if(id=="menu-about") {
+            $("#about").removeClass("wow fadeInUp");
+            $("#about").removeAttr("style");
+        }
+    }
+});
+
+/**
+ * Animaciones
+ */
+$(window).load(function() {
+    $('#preloader').delay(900).fadeOut('slow');
+});
+
+$("#apps").animate({
+    width: "60%"
+}, 3000)
+
+$("#html").animate({
+    width: "83%"
+}, 3000);
+
+$("#analitica").animate( {
+    width: "70%"
+}, 3000);
+
+$("#javascript").animate( {
+    width: "80%"
+}, 3000);
+
+$("#php").animate( {
+    width: "65%"
+}, 3000);
+
+$("#node").animate( {
+    width: "77%"
+}, 3000);
+
+$(".services .service i").mouseover(function() {
+    $(this).addClass("animated bounce");
+});
+$(".services .service i").mouseout(function() {
+    $(this).removeClass("animated bounce");
+});
+
+
+/**
+ * Parcear URL
+ */
+function getGET() {
+    var loc=document.location.href;
+    if(loc.indexOf('?')>0) {
+        var getString=loc.split('?')[1];
+        var GET=getString.split('&');
+        var get= {}
+        ;
+        for(var i=0, l=GET.length;
+        i<l;
+        i++) {
+            var tmp=GET[i].split('=');
+            get.url=unescape(decodeURI(tmp[1]));
+        }
+        return get;
+    }
+}
+
+getGET();
+
+/**
+ * TABS
+ */
+$(document).ready(function() {
+    $('#tab-container').easytabs( {
+        updateHash: false, animate: true, transitionIn: 'fadeIn', transitionOut: 'fadeOut', tabActiveClass: 'active', transitionInEasing: 'linear', transitionOutEasing: 'linear'
+    });
+});
+
+// Portafolio
+$(window).load(function() {
+    $('#wrapper').bind('easytabs:after', function() {
+        $(".portfolio_items").isotope( {
+            itemSelector: '.single_item', layoutMode: 'fitRows', columnWidth: '.col-sm-3'
+        });
+        $('.portfolio_filter ul li').click(function() {
+            $(".portfolio_filter ul li").removeClass("select-cat");
+            $(this).addClass("select-cat");
+            var selector=$(this).attr('data-filter');
+            $(".portfolio_items").isotope( {
+                filter:selector, animationOptions: {
+                    duration: 750, easing: 'linear', queue: false,
+                }
+            });
+            return false;
+        });
+        $('.image-link').magnificPopup( {
+            type: 'image'
+        });
+    });
+});
